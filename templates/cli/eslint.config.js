16f0e1b58ac4b6693e07743f3f21277ed7ed07c8
Roshan Singh <alcanivorax@users.noreply.github.com>
2026-01-22 21:20:34 +0530

    initial setup

---
+++ templates/cli/eslint.config.js
@@ -0,0 +1,36 @@
+import tseslint from '@typescript-eslint/eslint-plugin'
+import parser from '@typescript-eslint/parser'
+
+export default [
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      parser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tseslint,
+    },
+    rules: {
+      // correctness
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        { argsIgnorePattern: '^_' },
+      ],
+
+      'no-shadow': 'off',
+      '@typescript-eslint/no-shadow': 'error',
+
+      'no-redeclare': 'off',
+      '@typescript-eslint/no-redeclare': 'error',
+
+      // sanity
+      'no-console': 'off',
+      'prefer-const': 'error',
+    },
+  },
+]
